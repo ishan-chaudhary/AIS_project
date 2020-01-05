e9198623b4b6bd13606280e89d611a4caca29bf3
Patrick <patrickfmaus@gwu.edu>
2020-01-04 21:03:40 -0500

    clean up of files and consolidation

--- db_creation.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE ais_data
-    WITH
-    OWNER = patrickmaus
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.UTF-8'
-    LC_CTYPE = 'en_US.UTF-8'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
