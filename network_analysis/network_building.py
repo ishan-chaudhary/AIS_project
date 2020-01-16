9402dab341344cf7f2659917833f703a9a02cbb8
Patrick <patrickfmaus@gwu.edu>
2020-01-16 17:02:07 -0500

    imporved network analysis.
    multiple problems in reducing port_activity identified and fiexed.  tested out implementation for one mmsi, but had to use python.  could not get SQL solution.  The problem was the the group by in sql clustered all activity at port or at sea, even if they were seperated by other visits.  Right now, I'm re-creating the entire ship_position table which is inefficent.  need to add columns to original table and update.
    
    Still need to iterate through all mmsi and process in python and then save the network components somewhere.  also still need to work on reading in edge attributes.

--- network_analysis/network_building.py
+++ network_analysis/network_building.py
@@ -4,47 +4,50 @@ import matplotlib as plt
 import networkx as nx
 
 #%% Make and test conn and cursor
-conn = psycopg2.connect(host="localhost",database="ais_data")
+conn = psycopg2.connect(host="localhost",database="ais_test")
 c = conn.cursor()
 if c:
     print('Connection is good.')
 c.close()
 
 #%% Get the df from the database
-port_df = pd.read_sql('select * from port_activity_reduced', conn)
+df = pd.read_sql("""select * from port_activity_sample 
+                      where mmsi = '212917000'""", conn)
+df.fillna(value=0, inplace=True)
+df.drop('geog', axis=1, inplace=True)
 
 #%% restructure the df to be ingested by networkx.  
 # Each row needs to be start node, end node, and then attributes
+edge_list = []
+attr_list = []
+position_count = 0
+origin = df['port_id'].iloc[0]
+depart_time = df['time'].iloc[0]
 
-
-#%%
-
-
-port_list = []
-for idx in range(len(port_df)):
-    try:
-        origin = port_df.iloc[idx, 4]
-        destination = port_df.iloc[idx + 1, 4]
-        depart_time = port_df.iloc[idx, 1]
-        arrival_time = port_df.iloc[idx + 1, 0]
-        
-        port_list.append((origin, destination, {'depart_time': depart_time}))
-        
-    except: 
-        print(idx)
-        continue
+for idx in (range(len(df)-1)):
+    if df['port_id'].iloc[idx] == df['port_id'].iloc[idx+1]:
+        position_count += 1
+      
+    else:
+        destination = df['port_id'].iloc[idx+1]
+        arrival_time = df['time'].iloc[idx+1]
+        if position_count > 10:
+            edge_list.append([origin, destination])
+            attr_list.append((origin, destination,
+                          [{'depart_time': depart_time},
+                          {'arrival_time' : arrival_time}, 
+                          {'position_count' : position_count}]))
+            position_count = 0
+            origin = df['port_id'].iloc[idx+1]
+            depart_time = df['time'].iloc[idx]
+            
+        else: continue
 
 #%%
 
 G = nx.MultiDiGraph()
-G.add_edges_from(port_list)
+G.add_edges_from(edge_list)
 
-
-
-
-#%%
-for k, v in G.degree():
-    print (k,v)
 
 
 
