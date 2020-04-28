61ae31452004e502c5299e01e33708009bcc3c1f
Patrick <patrickfmaus@gwu.edu>
2020-04-27 23:16:00 -0400

    speed test ais with 160mb buffers and 400mb work_mem.  clocked in at about 30 mins.

--- DBSCAN/dbscan_speed_tests.sql
+++ DBSCAN/dbscan_speed_tests.sql
@@ -4,4 +4,10 @@
     minpoints := 1000) over () as clust_id
     FROM ship_position_sample;
 	
+	--local
 	--4000mb shared buffers, 2000mb work mem: 23mins 29secs
+	--8000mb shared buffers, 2000mb work mem: 22mins 28secs
+	
+	show work_mem;
+	-- aws
+	--160mb shared buffers, work mem 400mb
