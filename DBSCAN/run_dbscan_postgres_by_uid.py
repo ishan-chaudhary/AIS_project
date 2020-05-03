faa7e5f101656dfb4f97d62e9a429dcf510a17fe
Patrick <patrickfmaus@gwu.edu>
2020-05-02 22:28:16 -0400

    successfully broke out dbscan to cluster each ship individually and iterating through a smaller set of hyperparameters on the local server.

---
+++ DBSCAN/run_dbscan_postgres_by_uid.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 23:03:35 2020
+
+@author: patrickmaus
+"""
+
+#time tracking
+import datetime
+
+# db admin
+import psycopg2
+from sqlalchemy import create_engine
+
+#%%
+import aws_credentials as a_c
+user = a_c.user
+host = a_c.host
+port = '5432'
+database = 'aws_ais_clustering'
+password = a_c.password
+
+aws_conn = psycopg2.connect(host=host,database=database, user=user,password=password)
+aws_c = aws_conn.cursor()
+if aws_c:
+    print('Connection to AWS is good.'.format(database))
+else: print('Connection failed.')
+aws_c.close()
+
+
+# def create_aws_engine(database):
+#     import aws_credentials as a_c
+#     user = a_c.user
+#     host = a_c.host
+#     port = '5432'
+#     password = a_c.password
+#     try:
+#         aws_engine = create_engine('postgresql://{}:{}@{}:{}/{}'.format(user, password, host, port, database))
+#         print('AWS Engine created and connected.')
+#         return aws_engine
+#     except:
+#         print('AWS Engine creation failed.')
+#         return None
+
+# aws_engine = create_aws_engine('aws_ais_clustering')
+
+#%%
+database='ais_test'
+loc_conn = psycopg2.connect(host="localhost",database=database)
+c = loc_conn.cursor()
+if c:
+    print('Connection to {} is good.'.format(database))
+else:
+    print('Error connecting.')
+c.close()
+
+    # # drop table if an old one exists
+    # c = conn.cursor()
+    # c.execute("""DROP TABLE IF EXISTS {}""".format(new_table_name))
+    # conn.commit()
+    # c.close()
+
+#%%
+source_table = 'ship_position_sample'
+c = loc_conn.cursor()
+c.execute("""SELECT DISTINCT(mmsi) FROM {};""".format(source_table))
+mmsi_list = c.fetchall()
+c.close()
+
+
+
+#%%
+def postgres_dbscan(source_table, eps_km, min_samples, mmsi_list, conn):
+
+    #this formulation will yield epsilon based on km desired
+    kms_per_radian = 6371.0088
+    eps = eps_km / kms_per_radian
+
+    new_table_name = ('dbscan_results_by_mmsi_' + str(eps_km).replace('.','_') +
+                      '_' + str(min_samples))
+    
+    c = conn.cursor()
+    c.execute("""CREATE TABLE IF NOT EXISTS {}
+    (
+        id integer,
+        mmsi text,
+        lat numeric,
+        lon numeric,
+        clust_id integer
+    );""".format(new_table_name))
+    conn.commit()
+
+    print("""Starting processing on DBSCAN with eps_km={} and
+          min_samples={} """.format(str(eps_km), str(min_samples)))
+    
+    for mmsi in mmsi_list:          
+        dbscan_sql = """INSERT INTO {} (id, mmsi, lat, lon, clust_id)
+        SELECT id, mmsi, lat, lon,
+        ST_ClusterDBSCAN(Geometry(geog), eps := {}, minpoints := {})
+        over () as clust_id
+        FROM {}
+        WHERE mmsi = '{}';""".format(new_table_name, str(eps), str(min_samples), source_table, mmsi[0])
+        # execute dbscan script
+        c = conn.cursor()
+        c.execute(dbscan_sql)
+        conn.commit()
+        c.close()
+        print('MMSI {} complete.'.format(mmsi[0]))
+    
+    print('DBSCAN complete, {} created'.format(new_table_name))
+
+
+def make_tables_geom(table, conn):
+    # add a geom column to the new table and populate it from the lat and lon columns
+    c = conn.cursor()
+    c.execute("""ALTER TABLE {} ADD COLUMN
+                geom geometry(Point, 4326);""".format(table))
+    conn.commit()
+    c.execute("""UPDATE {} SET
+                geom = ST_SetSRID(ST_MakePoint(lon, lat), 4326);""".format(table))
+    conn.commit()
+    c.close()
+
+
+#%%
+
+print('Function run at:', datetime.datetime.now())
+epsilons = [2, 5, 7, 10]
+samples = [50, 100, 250, 500]
+
+for e in epsilons:
+    for s in samples:
+
+        loc_conn = psycopg2.connect(host="localhost",database=database)
+        tick = datetime.datetime.now()
+        # pass the epsilon in km.  the function will convert it to radians
+        postgres_dbscan('ship_position_sample', e, s, mmsi_list, loc_conn)
+
+        #timekeeping
+        tock = datetime.datetime.now()
+        lapse = tock - tick
+        print ('Time elapsed: {}'.format(lapse))
