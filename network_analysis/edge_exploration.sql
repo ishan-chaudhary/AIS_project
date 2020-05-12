f047003250832d37b30cdec95a166c9cb756f67d
Patrick <patrickfmaus@gwu.edu>
2020-05-12 11:17:34 -0400

    Create edge_exploration.sql

---
+++ network_analysis/edge_exploration.sql
@@ -0,0 +1,11 @@
+select origin, (count(origin))
+from cargo_edges
+group by origin
+order by count desc
+
+select count(distinct(mmsi))
+from cargo_edges
+
+select count(distinct(mmsi))
+from ship_ports
+where nearest_port_dist_km < 5
