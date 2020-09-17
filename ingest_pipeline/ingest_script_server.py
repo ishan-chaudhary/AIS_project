709d4f54d04620e3f5c09b4fa654d06554e761c5
Patrick <patrickfmaus@gwu.edu>
2020-09-16 20:15:47 -0400

    added ship status, cog, and sog to the ingest script, as well as boolean columns for 'moored', 'anchored', 'underway' to use for decision trees or neural net as identified in #16.  also used the enumerated type column to store the long strings within the status messages.

--- ingest_pipeline/ingest_script_server.py
+++ ingest_pipeline/ingest_script_server.py
@@ -24,15 +24,40 @@ import gsta_config
 conn = gsta.connect_psycopg2(gsta_config.colone_cargo_params)
 loc_engine = gsta.connect_engine(gsta_config.colone_cargo_params)
 
-
-#%%
-# Create "uid_position" table in the  database.
+# %%
+# Create enumerated type for the strings in the status column
 c = conn.cursor()
+c.execute("""CREATE TYPE ship_status AS ENUM (
+'at anchor',
+'reserved for future use (10)',
+'AIS-SART (active); MOB-AIS; EPIRB-AIS',
+'reserved for future use (13)',
+'reserved for future use (9)',
+'aground',
+'undefined',
+'under way sailing',
+'power-driven vessel towing astern',
+'power-driven vessel pushing ahead or towing alongside',
+'under way using engine',
+'restricted maneuverability',
+'engaged in fishing',
+'constrained by her draught',
+'not under command',
+'moored');""")
+conn.commit()
+
+# Create "imported_data" table in the  database.
 c.execute("""CREATE TABLE IF NOT EXISTS imported_data
 (uid text, 
 time timestamp, 
 lat numeric, 
-lon numeric);""")
+lon numeric,
+sog numeric,
+cog numeric, 
+status ship_status,
+anchored boolean,
+moored boolean,
+underway boolean);""")
 conn.commit()
 c.close()
 # %% drop other tables
@@ -46,8 +71,7 @@ current_folder = '/home/patrickfmaus/AIS_project'
 if not os.path.exists(current_folder + '/script_processing/logs'):
     os.makedirs(current_folder + '/script_processing/logs')
 
-
-#%%
+# %%
 ingest_folder = current_folder + '/script_processing'
 log_folder = current_folder + '/script_processing/logs'
 
@@ -64,20 +88,25 @@ log = open(error_log, 'a+')
 log.write('Starting overall processing at: {} \n'.format(first_tick.strftime("%Y_%m_%d_%H%M")))
 log.close()
 
-
 # Create "uid_position" table in the  database.
 c = conn.cursor()
-c.execute("""CREATE TABLE IF NOT EXISTS uid_positions
-(id serial, 
+c.execute("""CREATE TABLE IF NOT EXISTS uid_positions_status
+(id serial primary key, 
 uid text, 
 time timestamp, 
 geom geometry, 
 lat numeric, 
-lon numeric);""")
+lon numeric,
+sog numeric,
+cog numeric,
+status ship_status,
+anchored boolean,
+moored boolean,
+underway boolean);""")
 conn.commit()
 c.close()
 
-# %% create WPI table funtion
+# %% create WPI table function
 wpi_csv_path = current_folder + '/WPI_data/wpi_clean.csv'
 conn = gsta.connect_psycopg2(gsta_config.loc_cargo_params)
 
@@ -85,7 +114,7 @@ conn = gsta.connect_psycopg2(gsta_config.loc_cargo_params)
 def make_sites(conn, file=wpi_csv_path):
     c = conn.cursor()
     c.execute("""CREATE TABLE IF NOT EXISTS sites (
-                    site_id 	int,
+                    site_id 	int primary key,
                     region_no	int,
                     port_name	text,
                     country		text,
@@ -102,6 +131,7 @@ def make_sites(conn, file=wpi_csv_path):
     conn.commit()
     c.close()
     print('Sites created')
+
 
 make_sites(conn=conn)
 
@@ -128,7 +158,7 @@ def download_url(link, download_path, unzip_path, file_name, chunk_size=10485760
 
 
 # %% read in and parse the original file into new tables
-def parse_SQL(file_name, conn=conn):
+def parse_sql(file_name, conn=conn):
     c = conn.cursor()
     # need to clear out imported data first.
     c.execute("""TRUNCATE imported_data;""")
@@ -136,20 +166,37 @@ def parse_SQL(file_name, conn=conn):
     print('Copying data to temp table...')
     generator = pd.read_csv(file_name, chunksize=500000)
     for df in generator:
-        df = df[['MMSI', 'BaseDateTime', 'LAT', 'LON', 'VesselType']]
-        df.columns = ['uid', 'time', 'lat', 'lon', 'ship_type']
-        df = df[df['ship_type'].isin([70,71,72,73,74,75,76,77,78,79,1003,1004,1016])]
-        df['time'] = pd.to_datetime(df['time'])
+        # subset the df to just the columns needed for processing and rename them
+        df = df[['MMSI', 'BaseDateTime', 'LAT', 'LON', 'SOG', 'COG', 'Status', 'VesselType']]
+        df.columns = ['uid', 'time', 'lat', 'lon', 'sog', 'cog', 'status', 'ship_type']
+        # filter by just cargo ships as ship_type, then drop the column
+        df = df[df['ship_type'].isin([70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 1003, 1004, 1016])]
         df.drop('ship_type', inplace=True, axis=1)
+        # convert time to pandas datetime
+        df['time'] = pd.to_datetime(df['time'])
+        # set columns for anchored, moored, and underway, and set values to True when present
+        df.loc[df['status'] == 'at anchor', 'anchored'] = True
+        df.loc[df['status'] == 'moored', 'moored'] = True
+        df.loc[df['status'] == 'under way sailing', 'underway'] = True
+        df.loc[df['status'] == 'under way using engine', 'underway'] = True
+        df.loc[df['status'] == 'restricted maneuverability', 'underway'] = True
+        df.loc[df['status'] == 'constrained by her draught', 'underway'] = True
+        # write df back to the database
         df.to_sql(name='imported_data', con=loc_engine, if_exists='append', method='multi', index=False)
     print('Copying complete!')
     # this will only insert positions from cargo ship types
-    c.execute("""INSERT INTO uid_positions (uid, time, geom, lat, lon)
+    c.execute("""INSERT INTO uid_positions (uid, time, geom, lat, lon, sog, cog, status, anchored, moored, underway)
                 SELECT uid, 
                 time, 
                 ST_SetSRID(ST_MakePoint(lon, lat), 4326), 
                 lat, 
-                lon 
+                lon ,
+                sog,
+                cog,
+                status,
+                anchored,
+                moored,
+                underway
                 FROM imported_data;""")
     conn.commit()
     c.close()
@@ -164,10 +211,10 @@ def make_trips(new_table_name, source_table, conn):
     SELECT 
         uid,
         position_count,
-		ST_Length(ST_Transform(line, 4326))/1000 AS line_length_km,
-		first_date,
-		last_date,
-		last_date - first_date as time_diff,
+        ST_Length(ST_Transform(line, 4326))/1000 AS line_length_km,
+        first_date,
+        last_date,
+        last_date - first_date as time_diff,
         line
         FROM (
                 SELECT pos.uid as uid,
@@ -202,13 +249,15 @@ for month in range(1, 13):
         log = open(log_name, 'a+')
         log.write(f'Started month/zone {month}/{zone} at {datetime.datetime.now().strftime("%H%M")} \n')
         log.close()
+        # set the variable for this round of iteration
+        file_name = f'{str(year)}_{str(month).zfill(2)}_{str(zone).zfill(2)}'
+        download_path = f'{ingest_folder}/{file_name}.zip'
+        unzip_path = f'{ingest_folder}/{str(year)}'
+        url_path = f'https://coast.noaa.gov/htdata/CMSP/AISDataHandler/{str(year)}/'
+        url_file = f'AIS_{str(year)}_{str(month).zfill(2)}_Zone{str(zone).zfill(2)}'
+        link = url_path + url_file + '.zip'
+
         try:
-            file_name = f'{str(year)}_{str(month).zfill(2)}_{str(zone).zfill(2)}'
-            download_path = f'{ingest_folder}/{file_name}.zip'
-            unzip_path = f'{ingest_folder}/{str(year)}'
-            url_path = f'https://coast.noaa.gov/htdata/CMSP/AISDataHandler/{str(year)}/'
-            url_file = f'AIS_{str(year)}_{str(month).zfill(2)}_Zone{str(zone).zfill(2)}'
-            link = url_path + url_file + '.zip'
             print(f'Started file {file_name[-22:]} at {tick.strftime("%H%M")}.')
             # use the download_url function to download the zip file, unzip it, and
             # delete the zip file
@@ -218,7 +267,7 @@ for month in range(1, 13):
             # if there is only one csv file in the root folder, everything is working.
             if len((glob.glob((ingest_folder + '/**/*.csv'), recursive=True))) == 1:
                 file = glob.glob((ingest_folder + '/**/*.csv'), recursive=True)[0]
-                parse_SQL(file)
+                parse_sql(file)
                 print(f'Parsing complete for {file_name}.')
                 # delete the csv file
                 os.remove(file)
