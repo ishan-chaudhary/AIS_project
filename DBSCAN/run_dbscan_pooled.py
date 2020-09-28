ae9596dd94827e920cb69e2c3131d1571d0f42e7
Patrick <patrickfmaus@gwu.edu>
2020-09-28 16:58:39 -0400

    complete reqork of the run debscan function into more modular units.  supports other work across the project by having more modular funciton.  Transitioned functions to gnact module, but still need to publish gnact module to github for use on remote server.

--- DBSCAN/run_dbscan_pooled.py
+++ DBSCAN/run_dbscan_pooled.py
@@ -17,15 +17,15 @@ reload(gsta)
 import warnings
 warnings.filterwarnings('ignore')
 
-#%%
+# %%
 start_time = '2017-01-01 00:00:00'
-end_time = '2017-02-01 00:00:00'
+end_time = '2018-01-01 00:00:00'
 
 # %% Create needed accessory tables and ensure they are clean.  also get uid list
 conn = gsta.connect_psycopg2(gsta_config.colone_cargo_params, print_verbose=False)
 c = conn.cursor()
 
-#Create "clustering_results" table in the database.
+# Create "clustering_results" table in the database.
 c.execute(f"""CREATE TABLE IF NOT EXISTS clustering_results 
         AS (SELECT id from uid_positions
         where time between '{start_time}' and '{end_time}'
@@ -47,6 +47,7 @@ print(f'{str(len(uid_list))} total uids returned.')
 
 c.close()
 conn.close()
+
 
 # %%
 def make_uid_tracker(conn_pg):
@@ -83,59 +84,70 @@ def add_to_uid_tracker(uid, conn_pg):
     uids_len = (c_pg.fetchone())
     c_pg.close()
     conn_pg.close()
-    return uids_len[0] # remember its a tuple from the db.  [0] gets the int
+    return uids_len[0]  # remember its a tuple from the db.  [0] gets the int
 
 
-# %%
-def sklearn_dbscan(uid, eps, min_samp, print_verbose=False):
+def get_uid_posits(uid, engine_pg, start_time='2017-01-01 00:00:00', end_time='2018-01-01 00:00:00'):
+    read_sql = f"""SELECT id, lat, lon
+                FROM uid_positions
+                WHERE uid = '{uid[0]}'
+                AND time between '{start_time}' and '{end_time}'
+                ORDER by time"""
+    df_posits = pd.read_sql_query(read_sql, con=engine_pg)
+    return df_posits
+
+
+def get_clusters(df, eps_km, min_samp, method):
+    # format data for dbscan
+    X = (np.radians(df.loc[:, ['lon', 'lat']].values))
+    x_id = df.loc[:, 'id'].values
+    try:
+        if method == 'dbscan':
+            # execute sklearn's DBSCAN
+            dbscan = DBSCAN(eps=eps_km/6371, min_samples=min_samp, algorithm='ball_tree',
+                            metric='haversine', n_jobs=1)
+            dbscan.fit(X)
+            results_dict = {'id': x_id, 'clust_id': dbscan.labels_}
+        if method == 'optics':
+            # execute sklearn's OPTICS
+            # 5km in radians is max eps
+            optics = OPTICS(max_eps=eps_km/6371, min_samples=min_samp, metric='euclidean', cluster_method='xi',
+                            algorithm='kd_tree', n_jobs=1)
+            optics.fit(X)
+            results_dict = {'id': x_id, 'clust_id': optics.labels_}
+        if method not in ['optics', 'dbscan']:
+            print("Error.  Method must be 'dbscan' or 'optics'.")
+            return None
+    except Exception as e:
+        print(f'UID {uid[0]} error in clustering.')
+        print(e)
+        return None
+    # gather the output as a dataframe
+    df_results = pd.DataFrame(results_dict)
+    # drop all -1 clust_id, which are all points not in clusters
+    df_results = df_results[df_results['clust_id'] != -1]
+    return df_results
+
+
+#%%
+def pooled_clustering(uid, eps_km, min_samp, method, print_verbose=True):
     iteration_start = datetime.datetime.now()
+    dest_column = f"{method}_{str(eps_km).replace('.', '_')}_{min_samp}"
     temp_table_name = f'temp_{str(uid[0])}'
 
     engine_pg = gsta.connect_engine(gsta_config.colone_cargo_params, print_verbose=False)
     conn_pg = gsta.connect_psycopg2(gsta_config.colone_cargo_params, print_verbose=False)
     c_pg = conn_pg.cursor()
 
-    # next get the data for the uid
-    read_sql = f"""SELECT id, lat, lon
-                FROM uid_positions
-                WHERE uid = '{uid[0]}'
-                AND time between '{start_time}' and '{end_time}'
-                ORDER by time"""
-    df = pd.read_sql_query(read_sql, con=engine_pg)
-
-    # format data for dbscan
-    X = (np.radians(df.loc[:, ['lon', 'lat']].values))
-    x_id = df.loc[:, 'id'].values
+    df_posits = get_uid_posits(uid, engine_pg)
+    df_results = get_clusters(df_posits, eps_km=eps_km, min_samp=min_samp, method=method)
 
     try:
-        if method == 'dbscan':
-            # execute sklearn's DBSCAN
-            dbscan = DBSCAN(eps=eps, min_samples=min_samp, algorithm='ball_tree',
-                            metric='haversine', n_jobs=1)
-            dbscan.fit(X)
-            results_dict = {'id': x_id, 'clust_id': dbscan.labels_}
-
-        if method == 'optics':
-            # execute sklearn's OPTICS
-            # 5km in radians is max eps
-            optics = OPTICS(max_eps=eps, min_samples=min_samp,  metric='euclidean', cluster_method='xi',
-                            algorithm='kd_tree', n_jobs=1)
-            optics.fit(X)
-            results_dict = {'id': x_id, 'clust_id': optics.labels_}
-
-        else:
-            print("Error.  Method must be 'dbscan' or 'optics'.")
-            return
-
-        # gather the output as a dataframe
-        df_results = pd.DataFrame(results_dict)
-        # drop all -1 clust_id, which are all points not in clusters
-        df_results = df_results[df_results['clust_id'] != -1]
         # write results to database in a temp table with the uid in the name
         sql_drop_table = f"""DROP TABLE IF EXISTS {temp_table_name};"""
         c_pg.execute(sql_drop_table)
         conn_pg.commit()
-        sql_create_table = f"""CREATE TEMPORARY TABLE {temp_table_name}
+        sql_create_table = f"""CREATE TABLE {temp_table_name}
                            (id int, 
                            clust_id int);"""
         c_pg.execute(sql_create_table)
@@ -144,35 +156,30 @@ def sklearn_dbscan(uid, eps, min_samp, print_verbose=False):
                           if_exists='append', method='multi', index=False)
         # take the clust_ids from the temp table and insert them into the temp table
         sql_update = f"UPDATE clustering_results AS c " \
-                     f"SET {params_name} = clust_id " \
+                     f"SET {dest_column} = clust_id " \
                      f"FROM {temp_table_name} AS t WHERE t.id = c.id"
         c_pg.execute(sql_update)
         conn_pg.commit()
-        c_pg.close()
 
     except Exception as e:
-        print(f'UID {uid[0]} error in clustering or writing clustering results to the database.')
+        print(f'UID {uid[0]} error in writing clustering results to the database.')
         print(e)
 
     # delete the temp table
-    c_pg = conn_pg.cursor()
-    c_pg.execute(f'DROP TABLE IF EXISTS {temp_table_name};')
+    c_pg.execute(sql_drop_table)
     conn_pg.commit()
-    # add the uid to the tracker and get current uid count from tracker
-    uids_completed = add_to_uid_tracker(uid, conn_pg)
     c_pg.close()
-
-    # delete the connections
+    # close the connections
     engine_pg.dispose()
     conn_pg.close()
 
     if print_verbose == True:
         print(f'UID {uid[0]} complete in ', datetime.datetime.now() - iteration_start)
+        uids_completed = add_to_uid_tracker(uid, conn_pg)
         percentage = (uids_completed / len(uid_list)) * 100
         print(f'Approximately {round(percentage, 3)} complete this run.')
 
-
-def postgres_dbscan(uid, eps, min_samp):
+def postgres_dbscan_reworked(uid, eps_km, min_samp):
     """
     A function to conduct dbscan on the server for a global eps and min_samples value.
     Optimized for multiprocessing.
@@ -181,7 +188,7 @@ def postgres_dbscan(uid, eps, min_samp):
     :param uid:
     :return:
     """
-    #iteration_start = datetime.datetime.now()
+    # iteration_start = datetime.datetime.now()
     # execute dbscan script
     dbscan_postgres_sql = f"""
     UPDATE clustering_results as c 
@@ -211,16 +218,15 @@ def postgres_dbscan(uid, eps, min_samp):
 first_tick = datetime.datetime.now()
 print('Starting Processing at: ', first_tick.time())
 # for optics, just put the max eps in for list of epsilons.
-epsilons = [5]
-min_samples = [25]
-#min_samples = [25, 50, 100, 200, 300, 400, 500]
+epsilons_km = [5]
+min_samples = [25, 50, 100, 200, 300, 400, 500]
 method = 'optics'
 
-for eps_km in epsilons:
+
+for eps_km in epsilons_km:
     for min_samp in min_samples:
         iteration_start = datetime.datetime.now()
-        eps = eps_km / 6371.0088 #kms per radian
-        params_name = f"{method}_{str(eps_km).replace('.','_')}_{min_samp}"
+        params_name = f"{method}_{str(eps_km).replace('.', '_')}_{min_samp}"
         print(f'Starting processing for {params_name}...')
 
         conn = gsta.connect_psycopg2(gsta_config.colone_cargo_params, print_verbose=False)
@@ -240,10 +246,9 @@ for eps_km in epsilons:
         # execute the function with pooled workers
         with Pool(20) as p:
             try:
-                p.starmap(sklearn_dbscan, zip(uid_list, repeat(eps), repeat(min_samp)))
+                 p.starmap(pooled_clustering, zip(uid_list, repeat(eps_km), repeat(min_samp), repeat(method)))
             except Exception as e:
-                print (e)
-
+                print(e)
 
         print(f'Method {params_name} complete in ', datetime.datetime.now() - iteration_start)
 
@@ -251,84 +256,3 @@ last_tock = datetime.datetime.now()
 lapse = last_tock - first_tick
 print('Processing Done.  Total time elapsed: ', lapse)
 
-
-
-
-
-
-#
-# #%%
-# conn = gsta.connect_psycopg2(gsta_config.colone_cargo_params)
-# uid = ('477478700',) # 40,000 rows is super fast as sklearn
-# first_tick = datetime.datetime.now()
-# print('Starting Processing at: ', first_tick.time())
-# print('Working on uid:', uid[0])
-# iteration_start = datetime.datetime.now()
-# engine = gsta.connect_engine(gsta_config.colone_cargo_params, print_verbose=False)
-# # next get the data for the uid
-# read_sql = f"""SELECT id, lat, lon
-#              FROM uid_positions
-#              WHERE uid = '{uid[0]}'
-#              ORDER by time"""
-# df = pd.read_sql_query(read_sql, con=engine)
-#
-# last_tock = datetime.datetime.now()
-# lapse = last_tock - first_tick
-# print('Processing Done.  Total time elapsed: ', lapse)
-# #%%
-# first_tick = datetime.datetime.now()
-# print('Starting Processing at: ', first_tick.time())
-#
-# # format data for dbscan
-# X = (np.radians(df.loc[:, ['lon', 'lat']].values))
-# x_id = df.loc[:, 'id'].values
-# #%%
-# optics = OPTICS(min_samples=250, algorithm='ball_tree', metric='euclidean')
-# optics.fit(X)
-#
-# #%%
-#
-# #%%
-# # execute sklearn's DBSCAN
-# dbscan = DBSCAN(eps=eps, min_samples=min_samples, algorithm='ball_tree',
-#                 metric='haversine', n_jobs=1)
-# dbscan.fit(X)
-# # gather the output as a dataframe
-# results_dict = {'id': x_id, 'clust_id': dbscan.labels_}
-#
-# #%%
-# df_results = pd.DataFrame(results_dict)
-# # drop all -1 clust_id, which are all points not in clusters
-# df_results = df_results[df_results['clust_id'] != 1]
-#
-# last_tock = datetime.datetime.now()
-# lapse = last_tock - first_tick
-# print('Processing Done.  Total time elapsed: ', lapse)
-# #%%
-# first_tick = datetime.datetime.now()
-# print('Starting Processing at: ', first_tick.time())
-#
-# # write results to database in a temp table with the uid in the name
-# df_results.to_sql(name=f'temp_{str(uid[0])}', con=engine,
-#                   if_exists='replace', method='multi', index=False)
-# engine.dispose()
-#
-# # take the clust_ids from the temp table and insert them into the
-# conn = gsta.connect_psycopg2(gsta_config.colone_cargo_params, print_verbose=False)
-# sql_update = f"UPDATE clustering_results AS c " \
-#              f"SET {method}_{eps_km}_{min_samples} = clust_id " \
-#              f"FROM temp_{str(uid[0])} AS t WHERE t.id = c.id"
-# c = conn.cursor()
-# c.execute(sql_update)
-# conn.commit()
-#
-# c.execute(f'DROP TABLE temp_{str(uid[0])};')
-# conn.commit()
-# c.close()
-# conn.close()
-#
-# last_tock = datetime.datetime.now()
-# lapse = last_tock - first_tick
-# print('Processing Done.  Total time elapsed: ', lapse)
-#
-# print(f'UID {uid[0]} complete in:', datetime.datetime.now() - iteration_start)
